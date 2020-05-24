a520a362da70eebdfd8ab99b4d0197a657fdfb6c
Steph W <wilhelm.steph@gmail.com>
2020-05-24 20:18:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,13 +1,13 @@
 class Song
 
-  attr_accessor :name, :artists, :genres
+  attr_accessor :name, :artist, :genre
 
   @@count = 0
 
-  def initialize(name, artists, genres)
+  def initialize(name, artist, genre)
     @name = name
-    @artists = artist
-    @genres = genre
+    @artist = artist
+    @genre = genre
     @@count += 1
   end
 
