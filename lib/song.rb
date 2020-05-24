77a26cfcfcd57d55e163fda7cbe9f9cd919787a9
Steph W <wilhelm.steph@gmail.com>
2020-05-24 20:56:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,25 +19,22 @@ class Song
     @name
   end
 
-  def self.artists
-    @@artists.uniq
-  end
-
-
-
   def self.count
     @@count
   end
 
-  def self.artist_count
-     @@artist_count = {}
+  def self.artists
+    @@artists.uniq
+  end
 
+  def self.artist_count
+     artist_count = {}
      @@artists.each do|artist|
        if artist_count[artist]
         artist_count[artist] += 1
-        else
+       else
         artist_count[artist] = 1
-        end
+       end
       end
       artist_count
     end
@@ -46,9 +43,8 @@ class Song
       @@genres.uniq
     end
 
-
     def self.genre_count
-      @@genre_count = {}
+      genre_count = {}
       @@genres.each do |genre|
         if genre_count[genre]
           genre_count[genre] += 1
