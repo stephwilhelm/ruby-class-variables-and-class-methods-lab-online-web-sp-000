37ea45b6636b57d62d3ec2fc0ba200fad3fa94ee
Steph W <wilhelm.steph@gmail.com>
2020-05-24 20:40:51 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,11 +34,15 @@ class Song
   def self.genre_count
    @@genre_count = {}
 
-   @@genres.each {|genre| @@genre_count[genre] += 1}
-
-   return @@genres
+   @@genres.each do|genre|
+     if !genre_count[genre]
+      genre_count[genre] = 1
+    else
+      genre_count[genre] += 1
+    end
   end
-
+    genre_count
+    
   def self.artist_count
 
   end
